1ec74d5b47b7a699939613cc097a267a4df4bb67
gcj <longgongsanri@163.com>
2016-12-04 18:55:02 +0800

    TabLayout标签导航

---
+++ app/src/main/java/tk/chuanjing/andnewfeaturemy/MyApp.java
@@ -0,0 +1,15 @@
+package tk.chuanjing.andnewfeaturemy;
+
+import android.app.Application;
+
+/**
+ * Created by ChuanJing on 2016/12/4.
+ */
+
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
